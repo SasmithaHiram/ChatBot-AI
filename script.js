2b4a1208d39f8a9dbe158bbf218338642baef91a
SasmithaHiram <sasmithahiram2003@gmail.com>
2024-12-09 18:09:39 +0530

    Set Gemini AI

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function send(){
-    let userInput=document.getElementById("txtF").value;
-    alert(userInput);
-    document.getElementById("user1").innerHTML=`<h3 id="user1">${userInput}</h3>`
-    document.getElementById("user2").innerHTML=`<h3 id="user2">${userInput}</h3>`
+function send() {
+    let userInput = document.getElementById("txtF").value;
+    document.getElementById("user1").innerHTML = `<h3 id="user1">${userInput}</h3>`
+
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const raw = JSON.stringify({
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": userInput
+                    }
+                ]
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyD1n-cB2kdG_bFXbDCB3GVjp2iIEPCUVb4", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result.candidates[0].content.parts[0].text)
+            document.getElementById("user2").innerHTML = `<h3 id="user2">${result.candidates[0].content.parts[0].text}</h3>`
+        })
+        .catch((error) => console.error(error));
 }
